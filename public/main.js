e8cfbee3bf5ab3d343711e4d9758137d2c244442
michaelnunzio <michaelnunzio12@gmail.com>
2019-01-31 18:24:01 -0500

    cont path

--- public/main.js
+++ public/main.js
@@ -1,3 +1,5 @@
+
+
 // carousel start
 // $(document).ready(function(){
 //     $(".carousel").carousel({fullWidth:true}).css('height',  $(window).height());
@@ -12,7 +14,45 @@ $( document ).ready(function() {
     $(".gittt").addClass("load");
     $('.sidenav').sidenav()
 
+    $(".contForm").click(function(){
+
+   
+        var someText = "Submmitted! I'll Reach Out Soon";
+        var newDiv = $("<div class='wow'>").append(someText);
+        console.log(someText)
+            
+            $(".subbbed").append(newDiv);
+            console.log("subbed")
+            
+            setTimeout(function() {
+                document.location.reload(true);
+        }, 10000);
+        
+        });
+
+
+    
 });
+
+
+
+// $(".contForm").click(function(){
+    
+
+//     var someText = "Submmitted! I'll Reach Out Soon";
+//     var newDiv = $("<div class='wow'>").append(someText);
+//     console.log(someText)
+//     $(".subbbed").append(newDiv);
+//     console.log("subbed")
+
+// });
+
+
+
+
+
+
+
 
 
 
